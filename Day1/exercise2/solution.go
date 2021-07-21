bc56188f4e55d020bf8e0303ea4655f08eca22ac
jaideepbhargava <87695902+jaideepbhargava@users.noreply.github.com>
2021-07-21 12:34:18 +0530

    Added complete solution for Excercise 2

--- Day1/exercise2/solution.go
+++ Day1/exercise2/solution.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
+type Node struct {
+	key string
+	left *Node
+	right *Node
+}
+
+func PreOrder(root *Node){
+
+	fmt.Println(root.key)
+
+	if root.left != nil {
+		PreOrder(root.left)
+	}
+
+	if root.right != nil {
+		PreOrder(root.right)
+	}
+}
+
+func PostOrder(root *Node){
+
+	if root.left != nil {
+		PostOrder(root.left)
+	}
+
+	if root.right != nil {
+		PostOrder(root.right)
+	}
+	fmt.Println(root.key)
+}
+
 func main(){
 
 	var inputExpression string
@@ -10,7 +41,10 @@ func main(){
 
 	for i:=0; i<len(inputExpression); i++{
 		if inputExpression == "+" || inputExpression == "-" || inputExpression == "*" || inputExpression == "/"{
+			fmt.Println("We have encountered an operator: Push into the stack")
 
+		}else {
+			fmt.Println("We have encountered an operand, POP the last operator from the stack")
 		}
 	}
 
